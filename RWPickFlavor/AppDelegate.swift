14ccf5f6dc97ae6e33e259624a565e8e9fd8f1ae
Gregory Jose <gregory.jose@novare.com.hk>
2016-07-15 14:50:12 +0800

    Initial commit

---
+++ RWPickFlavor/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  RWPickFlavor
+//
+//  Created by Gregory on 15/07/2016.
+//  Copyright © 2016 Novare Technologies. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Gregory on 15/07/2016.
+//  Copyright © 2016 Novare Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
